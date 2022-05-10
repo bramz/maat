8ffaac23a0eec3eb15e75d0ec113de11dbfff73d
Brock R <brock@saqus.com>
2022-05-10 19:57:26 -0400

    Added register and login componenets, stubbed out logic :zap:

--- manager/src/app/login/login.component.ts
+++ manager/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fb:FormBuilder,
+    
+
+  ) { }
 
   ngOnInit(): void {
   }
